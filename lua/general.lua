1b2850b50b614b60744919a10cb07e19f10a377b
cmdctl <anton.bozhinov@gmail.com>
2024-06-17 01:09:18 +0300

    updates

--- lua/general.lua
+++ lua/general.lua
@@ -91,11 +91,11 @@ lvim.autocommands = {
 
 -- quickfix window settings
 -- close quickfix menu after selecting choice
-vim.api.nvim_create_autocmd(
-  "FileType", {
-    pattern = { "qf" },
-    command = [[nnoremap <buffer> <CR> <CR>:cclose<CR>]]
-  })
+-- vim.api.nvim_create_autocmd(
+--   "FileType", {
+--     pattern = { "qf" },
+--     command = [[nnoremap <buffer> <CR> <CR>:cclose<CR>]]
+--   })
 
 -- additional filetypes
 vim.filetype.add({
